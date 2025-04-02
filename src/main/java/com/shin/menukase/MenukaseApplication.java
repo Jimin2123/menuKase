f7fd979be32b39be1a8c5fbb2a3d3156af16b23b
Jupiter <kjimin2123@gmail.com>
2025-04-02 21:51:27 +0900

    initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/java/com/shin/menukase/MenukaseApplication.java
@@ -0,0 +1,13 @@
+package com.shin.menukase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenukaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenukaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shin/menukase/MenukaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shin.menukase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenukaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
